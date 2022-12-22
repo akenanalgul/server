18f05c92c19ea8f865aef8164612b543fec4e0f5
Ahmet Kenan Algül <akenanalgul@gmail.com>
2022-12-22 21:06:50 +0300

    Aggregation the most important code block

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -101,3 +101,36 @@ export const likePost= async(req,res)=>{
 }
 
 // Get Timeline Posts
+export const getTimeLinePosts = async(req,res)=>{
+    const userId=req.params.id
+
+    try {
+        const currentUserPosts= await PostModel.find({userId:userId})
+         const followingPosts= await UserModel.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(userId)
+
+                }
+            },
+            {
+                $lookup: {
+                    from: "postst",
+                    localField: "following",
+                    foreignField: "userId",
+                    as: "followingPosts"
+                }
+            },
+            {
+                $project: {
+                    followingPosts : 1,
+                    _id: 0
+                }
+            }
+         ])
+         res.status(200).json(currentUserPosts.concat(followingPosts))
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
