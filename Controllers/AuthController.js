da301c7acf8d3f145da34db2922163b4749747b9
Ahmet Kenan Algül <akenanalgul@gmail.com>
2023-01-20 12:15:56 +0300

    new token

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,5 +1,6 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 // Registering a new User
 export const registerUser = async (req, res) => {
@@ -17,8 +18,12 @@ export const registerUser = async (req, res) => {
     {
       return res.status(400).json({message:"username is already registered!"})
     }
-    await newUser.save();
-    res.status(200).json(newUser);
+    const user= await newUser.save();
+
+    const token =jwt.sign({
+      username: user.username, id: user._id
+    },process.env.JWT_KEY,{expiresIn:'1h'})
+    res.status(200).json({user,token});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -36,7 +41,17 @@ export const loginUser= async(req, res) =>{
         if(user){
             const validity= await bcrypt.compare(password, user.password)
 
-            validity? res.status(200).json(user): res.status(400).json("Wrong Password")
+            if(!validity)
+            {
+              res.status(400).json("Wrong Password")
+            }
+            else
+            {
+              const token =jwt.sign({
+                username: user.username, id: user._id
+              },process.env.JWT_KEY,{expiresIn:'1h'})
+              res.status(200).json({user, token})
+            }
         }
         else
         {
